f4e899a8af20a75afa3e8bee4da29c6890ff08b7
Marc Fischer <fischerbootcamp@gmail.com>
2022-05-09 12:05:35 +0800

    Updated Server.js

--- server.js
+++ server.js
@@ -1,49 +1,23 @@
-const path = require('path')
 const express = require('express');
+const webRouter = require('./routes/web');
+const apiRouter = require('./routes/api');
+
 const app = express();
-const webRouter = require('./routes/web')
-const apiRouter = require('./routes/api')
 
-
-// config variables
-
+// config
 const PORT = process.env.PORT||3001;
+app.use( express.static('public'));
+
+app.use(express.json());
 
 app.use(webRouter);
 app.use(apiRouter);
 
-// call Middleware
-app.use( express.static('public'));
 
-app.get('/', (req, res) => {
 
-    const indexHtmlPath = path.join(__dirname, 'public', 'index.html');
-    console.log('aaaa')
-    res.sendFile(indexHtmlPath)
-})
 
-app.get('/notes', (req, res) => {
 
-const notesHtmlPath = path.join(__dirname, '..', 'public', 'notes.html')
-res.sendFile(notesHtmlPath);
-})
-
-app.use(express.json());
-
-// Run business logic
 app.listen(PORT, function(){
   console.log(`App is running on http://localhost:${PORT}`);
 });
- 
-
-
-
-
-
-
-
-
-
-
-
 
